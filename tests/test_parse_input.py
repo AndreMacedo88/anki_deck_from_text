8bd54ab69b12d03b272f71e6a38eeb32e791aefb
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2023-12-27 13:38:17 +0000

    test: :white_check_mark: add tests for UTF8 encoded input

--- tests/test_parse_input.py
+++ tests/test_parse_input.py
@@ -3,10 +3,27 @@ from anki_deck_from_text.parse_input import question_answer_split, generate_ques
 
 class TestQuestionAnswerSplit:
     line = "- answer = question"
-    question, answer = question_answer_split(line)
+    question, answer = question_answer_split(
+        line=line,
+        separator=" = "
+    )
 
     def test_answer(self):
         assert self.answer == "answer"
 
     def test_question(self):
         assert self.question == "question"
+
+
+class TestQuestionAnswerSplitUtf8:
+    line = "- änswer = questiön"
+    question, answer = question_answer_split(
+        line=line,
+        separator=" = "
+    )
+
+    def test_answer(self):
+        assert self.answer == "änswer"
+
+    def test_question(self):
+        assert self.question == "questiön"
