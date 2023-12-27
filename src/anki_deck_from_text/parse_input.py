ea44bdca7093cdd7a2cebb54666394fe7f435f8a
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2023-12-27 13:59:17 +0000

    feat: :sparkles: include a parameter to define a custom separator between question and answer

--- src/anki_deck_from_text/parse_input.py
+++ src/anki_deck_from_text/parse_input.py
@@ -1,14 +1,17 @@
 
-def question_answer_split(line):
+def question_answer_split(line, separator):
     line = line.lstrip("- ")
-    answer, question = line.split(" = ")
+    answer, question = line.split(separator)
     return question, answer
 
 
-def generate_question_answer_dict(input):
+def generate_question_answer_dict(input, separator):
     cards_details = dict()
-    with open(input) as FileObj:
+    with open(input, encoding="utf-8") as FileObj:
         for line in FileObj:
-            question, answer = question_answer_split(line)
+            question, answer = question_answer_split(
+                line=line.strip(),
+                separator=separator
+            )
             cards_details[question] = answer
     return cards_details
