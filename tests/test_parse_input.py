a537399d93e8706c3a39106c61067b4dc831bcf0
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2023-12-27 13:59:50 +0000

    test: :white_check_mark: test generate_question_answer_dict function

--- tests/test_parse_input.py
+++ tests/test_parse_input.py
@@ -1,4 +1,8 @@
+import os
 from anki_deck_from_text.parse_input import question_answer_split, generate_question_answer_dict
+
+
+PARENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 class TestQuestionAnswerSplit:
@@ -27,3 +31,18 @@ class TestQuestionAnswerSplitUtf8:
 
     def test_question(self):
         assert self.question == "questiön"
+
+
+class TestGenerateQuestionAnswerDict:
+    file_str = os.path.join(PARENT_DIR, "test_md.md")
+
+    def test_dict(self):
+        dict_expected = {
+            "the girls": "die Mädchen",
+            "the house": "das Haus"
+        }
+        dict_results = generate_question_answer_dict(
+            input=self.file_str,
+            separator=" = "
+        )
+        assert dict_results == dict_expected
