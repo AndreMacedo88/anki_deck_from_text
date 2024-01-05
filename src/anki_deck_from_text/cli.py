6c4113d011acdfff5070d49a0b58427d3dfa5477
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2024-01-05 20:08:01 +0000

    fix: :bug: fix lack of argument to name the output file

--- src/anki_deck_from_text/cli.py
+++ src/anki_deck_from_text/cli.py
@@ -4,10 +4,11 @@ from .generate_deck import generate_deck_type_sound, write_package
 
 
 @click.command()
-@click.argument('input')
+@click.argument("input")
+@click.argument("output")
 @click.argument("deck_name")
-@click.option('--separator', default=" = ", help="Characters that separate the front and back of the cards")
-def cli(input, deck_name, separator, out):
+@click.option("--separator", default=" = ", help="Characters that separate the front and back of the cards")
+def cli(input, deck_name, separator, output):
     """Generate and Anki deck from annotations on a text file"""
 
     question_answer_dict = generate_question_answer_dict(
@@ -20,6 +21,6 @@ def cli(input, deck_name, separator, out):
         deck_name=deck_name,
     )
 
-    write_package(deck, out)
+    write_package(deck, output)
 
     click.echo("Finished")
