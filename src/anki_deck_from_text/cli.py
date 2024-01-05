a2a4d8be794873c83b9833518eeec5a47ff347a1
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2024-01-05 19:45:45 +0000

    feat: :sparkles: update the CLI to write a Anki package at a location defined by argument `out`

--- src/anki_deck_from_text/cli.py
+++ src/anki_deck_from_text/cli.py
@@ -1,14 +1,25 @@
 import click
 from .parse_input import generate_question_answer_dict
+from .generate_deck import generate_deck_type_sound, write_package
 
 
 @click.command()
 @click.argument('input')
+@click.argument("deck_name")
 @click.option('--separator', default=" = ", help="Characters that separate the front and back of the cards")
-def cli(input, separator):
-    """Generate Anki decks and cards from annotations on a text file"""
-    generate_question_answer_dict(
+def cli(input, deck_name, separator, out):
+    """Generate and Anki deck from annotations on a text file"""
+
+    question_answer_dict = generate_question_answer_dict(
         input=input,
         separator=separator
     )
+
+    deck = generate_deck_type_sound(
+        question_answer_dict=question_answer_dict,
+        deck_name=deck_name,
+    )
+
+    write_package(deck, out)
+
     click.echo("Finished")
